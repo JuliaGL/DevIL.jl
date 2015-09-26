77fb9fad64109afe249ab2116d46ec6adafaaed1
Sdanisch <sdanisch@gmail.com>
2015-09-26 20:23:23 +0200

    use ZipFile for decompression

--- deps/build.jl
+++ deps/build.jl
@@ -1,26 +1,30 @@
 using Compat
+@windows_only using ZipFile
 @windows_only begin
 	t = round(Int, time())
 	if WORD_SIZE == 64
-		OS_ARCH = "x64"
 		# 64-bit version is not available in an end-user package, so we download the SDK
 		srcUrl = "http://downloads.sourceforge.net/project/openil/DevIL%20Windows%20SDK/1.7.8/DevIL-SDK-x64-1.7.8.zip?r=&amp;ts=$(t)&amp;use_mirror=auto_select"
-		fileName = "DevIL-SDK-x64-1.7.8.zip"
 	elseif WORD_SIZE == 32
-		OS_ARCH = "x86"
 		srcUrl = "http://downloads.sourceforge.net/project/openil/DevIL%20Win32/1.7.8/DevIL-EndUser-x86-1.7.8.zip?r=&amp;ts=$(t)&amp;use_mirror=auto_select"
-		fileName = "DevIL-EndUser-x86-1.7.8.zip"
 	else
 		error("DevIL: Unsupported Windows architecture: $(Sys.ARCH)")
 	end
-	dstDir = Pkg.dir("DevIL", "deps", OS_ARCH)
-	isdir(dstDir) || mkdir(dstDir)
-	dstFile = joinpath(dstDir, fileName)
-	zipped_lib = download(srcUrl, dstFile)
+	zipped_lib = download(srcUrl)
 	library_dir = Pkg.dir("DevIL", "deps", "bin")
 	isdir(library_dir) || mkdir(library_dir)
-	run(`"$JULIA_HOME\\7z.exe" e "$zipped_lib" *.dll -o"$library_dir" -y`)
-    library_name  = joinpath(library_dir, "DevIL.dll")
+	zr = ZipFile.Reader(zipped_lib)
+	lib_name = ""
+	for file in zr.files #uncompress
+		filename = basename(file.name)
+		if splitext(filename)[2] == ".dll"
+			contains(lowercase(filename), "devil") && (lib_name = filename)
+			open(joinpath(library_dir, filename), "w") do io
+				write(io, readall(file))
+			end
+		end
+	end
+    library_name  = joinpath(library_dir, lib_name)
 	open("deps.jl", "w") do io
 		println(io, "const libdevil = \"$(escape_string(library_name))\"")
 	end
